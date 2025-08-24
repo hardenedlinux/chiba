2c135ee5beccfee1d7df6bcb6e0f9a4d4dd41193
Nala Ginrut <mulei@gnu.org>
2025-08-24 14:23:23 +0900

    Upgrade to Artanis-1.3.0

---
+++ conf/plugins.scm
@@ -0,0 +1,8 @@
+;; This is a plugin configuration file, don't remove it!
+
+;; Uncomment following lines to enable plugins.
+;; Make sure you import the plugin modules here.
+(define-module (conf plugins)
+  #:use-module (artanis oht))
+
+;; (plugin-enable! name handler)
